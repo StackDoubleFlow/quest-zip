98bf39dac016a635547d34d57c25a054c5d78c64
StackDoubleFlow <ojaslandge@gmail.com>
2021-03-01 15:55:50 -0600

    Initial Commit

---
+++ Android.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+TARGET_ARCH_ABI := $(APP_ABI)
+
+rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+# If you would like to use more shared libraries (such as custom UI, utils, or more) add them here, following the format above.
+# In addition, ensure that you add them to the shared library build below.
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := zip
+LOCAL_SRC_FILES := $(call rwildcard,src/,*.c)
+LOCAL_LDLIBS += -llog -lz
+LOCAL_CFLAGS += -I"shared"
+LOCAL_C_INCLUDES += ./shared ./src
+include $(BUILD_SHARED_LIBRARY)
